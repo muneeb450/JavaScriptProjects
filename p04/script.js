a12f19cd7b7d880f6c9cd4189147f6e016e279df
muneeb450 <69414937+muneeb450@users.noreply.github.com>
2020-09-25 16:02:40 +0500

    updated p04
    script.js file updated

--- p04/script.js
+++ p04/script.js
@@ -6,38 +6,33 @@ const currTwoAmount = document.getElementById('amount-two');
 const flipButton = document.getElementById('flip');
 const rate = document.getElementById('rate');
 
-
 // Fetch exchange rate from 3rd Party API and update DOM
 // www.exchangerate-api.com
 function calculate() {
     const currencyOneCode = currOnePicker.value;
     const currencyTwoCode = currTwoPicker.value;
     
-    fetch(`https://v6.exchangerate-api.com/v6/868949209ae598452cf5c3b7/latest/${currencyOneCode}`)
-    .then( res => res.json() )
-    .then(data => {
-        // Get the Exchange Rate from API data
-        const exchangeRate = data.conversion_rates[currencyTwoCode]
-        console.log(exchangeRate);
+    fetch(`https://v6.exchangerate-api.com/v6/a43d02c063c1303f1c06c071/latest/${currencyOneCode}`)
+        .then( res => res.json() )
+        .then( data => {
+            // Get the Exchange Rate from API Data
+            const exchangeRate = data.conversion_rates[currencyTwoCode];
 
-        // Display the Converstion Rate
-        rate.innerText = `1 ${currencyOneCode} = ${exchangeRate} ${currencyTwoCode}`;
-        
-        // Apply Converstion Rate and Update Amount of Currency Two
-        currTwoAmount.value = (currOneAmount.value * exchangeRate).toFixed(2);
+            // Display the Conversion Rate
+            rate.innerText = `1 ${currencyOneCode} = ${exchangeRate} ${currencyTwoCode}`;
 
-
-    });
+            // Apply Conversion Rate and Update Amount of Currency Two
+            currTwoAmount.value = (currOneAmount.value * exchangeRate).toFixed(2);
+        });
 }
 
-// Flip Function for the Flip Button to reverse currency 
+// Flip Function for the Flip Button to reverse currency exchange
 function flip() {
     const temp = currOnePicker.value;
     currOnePicker.value = currTwoPicker.value;
     currTwoPicker.value = temp;
     calculate();
-}
-
+};
 
 // Event Listeners
 currOnePicker.addEventListener('change', calculate);
@@ -45,7 +40,5 @@ currTwoPicker.addEventListener('change', calculate);
 currOneAmount.addEventListener('input', calculate);
 currTwoAmount.addEventListener('input', calculate);
 flipButton.addEventListener('click', flip);
-
-
 
 calculate();
