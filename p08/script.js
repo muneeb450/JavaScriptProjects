c91b44a28927d17b79a96ac22faf7a4dd56db528
muneeb450 <69414937+muneeb450@users.noreply.github.com>
2021-01-22 21:21:22 +0500

    p08 name changed
    completed

--- p08/script.js
+++ p08/script.js
@@ -1,107 +1,123 @@
-const search = document.getElementById('search');
-const submit = document.getElementById('submit');
-const random = document.getElementById('random');
-const resultHeading = document.getElementById('result-heading');
-const mealContainer = document.getElementById('meals');
-const selectedMeal = document.getElementById('selected-meal');
+// Grab DOM elements from HTML
+const word = document.getElementById('word');
+const wrongLetters = document.getElementById('wrong-letters');
+const popup = document.getElementById('popup-container');
+const message = document.getElementById('win-lose');
+const restartButton = document.getElementById('restart');
+const notification = document.getElementById('slider-container');
 
-// Function to search meal from API and fetch the data
-function searchMeal(e) {
-    e.preventDefault()
+const hangmanParts = document.querySelectorAll('.hangman-part');
 
-    // Clear Selected Meal
-    selectedMeal.innerHTML = '';
+// An array of words to select from
+const wordPool = ['javascript','computer','hangman','facebook','youtube'];
 
-    // Get the search term from input field
-    const term = search.value;
-    
-    // Check if search term exists
-    if(term.trim()) {
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`
-                if(data.meals === null) {
-                    resultHeading.innerHTML = `<p>There are no search results for '${term}'. Please try a different search.</p>`
-                } else {
-                    mealContainer.innerHTML = data.meals.map( meal => `
-                        <div class="meal">
-                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                            <div class="meal-info" data-mealID="${meal.idMeal}">
-                                <h3>${meal.strMeal}</h3>
-                            </div>
-                        </div>
-                    `)
-                    .join('')
-                }
-            })
-    } else {
-        alert('Please enter a valid search.')
+// Selecting a word at random from the pool
+let selectedWord = wordPool[Math.floor(Math.random() * wordPool.length)];
+
+// Arrays to classify the input of the User
+const correctLetters = [];
+const incorrectLetters = [];
+
+// Function to display the word on the screen
+function displaySelectedWord() {
+    word.innerHTML = `
+        ${selectedWord
+            .split('')
+            .map(
+                letter => `
+                    <span class="letter">
+                        ${correctLetters.includes(letter) ? letter : '' }
+                    </span>
+                `
+            )
+            .join('')
+        }
+    `;
+
+    const wordText = word.innerText.replace(/\n/g, '');
+
+    if( wordText === selectedWord ) {
+        message.innerText = 'You won!';
+        popup.style.display = 'flex';
     }
 
-    // Clear Search Term
-    search.value = '';
+};
+
+// Function to display the sliding notification
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout( () => {notification.classList.remove('show');}, 3000);
 }
 
-// Function to fetch meal data using the meal id
-function getMealById(mealID) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-        .then( res => res.json())
-        .then( data => {
-            const meal = data.meals[0];
-            addMealToDOM(meal);
-        })
-}
-
-// Function to add a meal to DOM
-function addMealToDOM(meal) {
-    const ingredients = [];
-
-    for(let i = 1; i <= 20; i++) {
-        if(meal[`strIngredient${i}`]) {
-            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
-        } else {
-            break;
-        }
-    };
-
-    selectedMeal.innerHTML = `
-        <div class="selected-meal">
-            <h1>${meal.strMeal}</h1>
-            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-            <div class="selected-meal-info">
-                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : '' }
-                ${meal.strArea ? `<p>${meal.strArea}</p>` : '' }
-            </div>
-            <div class="main">
-                <p>${meal.strInstructions}</p>
-                <h2>Ingredients</h2>
-                <ul>
-                    ${ingredients.map( ingredient => `<li>${ingredient}</li>` ).join('')}
-                </ul>
-            </div>
-        </div>
+// Function to Update Incorrect Letters
+function updateWrongLetters() {
+    // Update the Display for Wrong Letters
+    wrongLetters.innerHTML = `
+    ${incorrectLetters.length > 0 ? `<p>Wrong</p>` : '' }
+    ${incorrectLetters.map( letter => `<span>${letter}</span>`)}
     `;
-}
 
-// Event Listeners
-// 1. Submit Form
-submit.addEventListener('submit', searchMeal);
+    // Display Hangman Parts on Incorrect Letter Input
+    hangmanParts.forEach( (part, index) => {
+        const errors = incorrectLetters.length;
 
-// 2. When Clicking a Meal
-mealContainer.addEventListener('click', e => {
-    const mealInfo = e.path.find( item => {
-        if(item.classList) {
-            return item.classList.contains('meal-info');
+        if ( index < errors ) {
+            part.style.display = 'block';
         } else {
-            return false
+            part.style.display = 'none';
         }
     });
 
-    if(mealInfo) {
-        const mealID = mealInfo.getAttribute('data-mealid');
-        getMealById(mealID);
+    // Show Popup if lost
+    if(incorrectLetters.length === hangmanParts.length) {
+        message.innerText = 'You lost!';
+        popup.style.display = 'flex';
     }
+}
 
-});
+// Event Handlers
+// 1. Event Handler for Keyboard Button Press
+window.addEventListener('keydown', e => {
+    if( e.keyCode >= 65 && e.keyCode <= 90 ) {
+        const letter = e.key;
+        
+        if( selectedWord.includes(letter) ) {
+            if(!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+                displaySelectedWord();
+            } else {
+                showNotification();
+            }
+        } else {
+            if(!incorrectLetters.includes(letter)) {
+                incorrectLetters.push(letter);
+                updateWrongLetters();
+            } else {
+                showNotification();
+            }
+        }
+
+    }
+})
+
+// 2. Event Listener for Restart Button
+restartButton.addEventListener('click', () => {
+    // Empty Arrays
+    correctLetters.splice(0);
+    incorrectLetters.splice(0);
+
+    // Get a new selected word from the pool
+    selectedWord = wordPool[Math.floor(Math.random() * wordPool.length)];
+
+    displaySelectedWord();
+
+    // Clear the Wrong Letters Div
+    updateWrongLetters();
+
+    // Hide the popup
+    popup.style.display = 'none';
+
+})
+
+displaySelectedWord();
